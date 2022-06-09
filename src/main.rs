b08f890f1950099691c3eef85e4e0a819ac29861
Ranko <ranko.stevanovich@gmail.com>
2022-06-09 22:20:20 +0300

    feat: read file source

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,44 @@
 use std::env;
+use std::io::{ BufReader, Read, self };
 use std::process;
+use std::path::Path;
+use std::fs::File;
+
+fn run(source: String) {
+    println!("{}", source);
+}
+
+
+fn run_file<P: ?Sized>(path: &P)
+where
+    P: AsRef<Path>,
+{
+    let f = File::open(path).expect("Unable to open the file");
+
+    let mut reader = BufReader::new(f);
+    let mut buffer = Vec::new();
+
+    reader.read_to_end(&mut buffer).unwrap();
+
+    let source = String::from_utf8(buffer).unwrap();
+
+    run(source);
+}
+
+fn run_prompt() {
+    // reads a line of input from the user on the command line and returns the result.
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let args_len = args.len();
 
-    if args_len > 1 {
+    if args_len > 2 {
         println!("Usage: jlox [script]");
         process::exit(64);
-    } else if args_len == 1 {
-        // runFile(args[0]);
+    } else if args_len == 2 {
+        run_file(Path::new(&args[1]));
     } else {
-        // runPrompt();
+        run_prompt();
     }
 }
